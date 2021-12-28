8bfe32fe8c876f841b08804818c2b04688be6027
MxBM <officialsmp305@gmail.com>
2021-12-28 01:32:29 -0800

    Use a factory to create the Flask Application.

--- web/__init__.py
+++ web/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-import web.views
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile("config.py", silent=True)
+
+    # Important to import views after the app is created.
+    from web import recipes
+
+    app.register_blueprint(recipes.bp)
+    app.add_url_rule("/", endpoint="index")
+
+    return app
---
+++ web/recipies.py
@@ -0,0 +1,11 @@
+
+import flask
+from flask import render_template
+
+
+bp = flask.Blueprint("recipes", __name__)
+
+
+@bp.route("/")
+def index():
+    return render_template("recipes/index.html")
