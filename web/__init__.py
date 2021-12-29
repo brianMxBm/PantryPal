1a6933d81ea8e1adc8c1d2d01c91bfb6a1b94c6f
MxBM <officialsmp305@gmail.com>
2021-12-29 03:51:00 -0800

    Load Enviornment Variables via DotEnv Package

--- web/__init__.py
+++ web/__init__.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask_cdn import CDN
 from whitenoise import WhiteNoise
 
+# dotenv is not present (nor does it need to be) in production.
+try:
+    import dotenv
+
+    dotenv.load_dotenv()
+except ImportError:
+    pass
+
 cdn = CDN()
 
 
@@ -16,3 +24,8 @@ def create_app():
 
     # Important to import views after the app is created.
     from web import recipes
+
+    app.register_blueprint(recipes.bp)
+    app.add_url_rule("/", endpoint="index")
+
+    return app
