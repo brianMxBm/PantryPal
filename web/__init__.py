baaec6bd1b84bd012ed00b40b4cb15ca738f91e8
MxBM <officialsmp305@gmail.com>
2021-12-29 03:36:42 -0800

    Utilize Whitenoise Package To Serve Static Files.

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from whitenoise import WhiteNoise
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile("config.py", silent=True)
+    app.wsgi_app = WhiteNoise(
+        app.wsgi_app, root="web/static/", prefix="static/")
 
     # Important to import views after the app is created.
     from web import recipes
