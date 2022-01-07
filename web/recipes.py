c0127d77bf8f589700486f12467ba3e3ae560d45
MxBM <officialsmp305@gmail.com>
2022-01-07 01:57:09 -0800

    Dynamically Load For The Recipe Modal Page.

--- web/recipes.py
+++ web/recipes.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 import flask
 from flask import render_template
 
 bp = flask.Blueprint("recipes", __name__)
 
+TEMPLATE_ROOT = Path(bp.root_path, "templates")
+MODAL_PAGES = []
+
+for p in (TEMPLATE_ROOT / "recipes/modal_pages").iterdir():
+    page = str(p.relative_to(TEMPLATE_ROOT).as_posix()), p.stem[3:].replace("_", " ").title()
+    MODAL_PAGES.append(page)
+
 
 @bp.route("/")
 def index():
-    return render_template("recipes/index.html")
+    return render_template("recipes/index.html", modal_pages=MODAL_PAGES)
