636083bba268a1f70ea17367220506954b443cc3
MxBM <officialsmp305@gmail.com>
2022-01-14 23:44:35 -0800

    Document API.js

--- web/static/api.js
+++ web/static/api.js
@@ -1,4 +1,15 @@
-export class Client {
+/**
+ * Makes requests to our API.
+ */
+ export class Client {
+    /**
+     * Create a URL for an endpoint with the given query parameters.
+     *
+     * @param {string} endpoint The API endpoint.
+     * @param {string} params The query parameters.
+     * @returns {URL} The built URL.
+     * @private
+     */
     _buildGetURL(endpoint, params) {
         const url = new URL(endpoint, window.location.href);
         Object.keys(params).forEach((key) =>
@@ -8,8 +19,15 @@ export class Client {
         return url;
     }
 
+    /**
+     * Perform a GET request.
+     *
+     * @param {string} endpoint The API endpoint.
+     * @param {string} params The query parameters.
+     * @returns {Promise<Response>} The response for the GET request.
+     */
     async get(endpoint, params) {
         const url = this._buildGetURL(`api/${endpoint}`, params);
         return await fetch(url);
     }
-} 
+}
