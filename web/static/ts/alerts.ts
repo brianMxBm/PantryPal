4134b8cc60373e2fac29240f7a78fe3aee90475f
MxBM <officialsmp305@gmail.com>
2022-01-21 23:59:41 -0800

    Add Classes That Represent Alerts From Observables

---
+++ web/static/ts/alerts.ts
@@ -0,0 +1,30 @@
+export const enum Severity {
+    WARNING,
+    ERROR,
+}
+
+export abstract class Alert {
+    public readonly message: string;
+    public readonly severity: Severity;
+
+    protected constructor(message: string, severity: Severity) {
+        this.message = message;
+        this.severity = severity;
+    }
+}
+
+export class WarningAlert extends Alert {
+    constructor(message: string) {
+        super(message, Severity.WARNING);
+    }
+}
+
+export class ErrorAlert extends Alert {
+    constructor(message: string) {
+        super(message, Severity.ERROR);
+    }
+}
+
+export class IngredientWarning extends ErrorAlert {}
+
+export class IngredientError extends WarningAlert {}
