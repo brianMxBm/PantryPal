ab58c2c0c46714236f6e951ae570d6b6dcb8999d
MxBM <officialsmp305@gmail.com>
2022-01-22 00:16:24 -0800

    Add A Controller For Recipes.

---
+++ web/static/ts/controllers/recipes.ts
@@ -0,0 +1,22 @@
+import {Recipes} from "../models/recipes";
+import {SelectedIngredients} from "../models/ingredients";
+
+export class RecipesController {
+    private _recipes: Recipes;
+    private _ingredients: SelectedIngredients;
+
+    constructor(recipesModel: Recipes, ingredientsModel: SelectedIngredients) {
+        this._recipes = recipesModel;
+        this._ingredients = ingredientsModel;
+    }
+
+    public async onSearch(
+        sort: string,
+        cuisine: string,
+        type: string,
+        maxReadyTime: string
+    ): Promise<void> {
+        const ingred = Array.from(this._ingredients.ingredients).join(",");
+        await this._recipes.update(ingred, sort, cuisine, type, maxReadyTime);
+    }
+}
