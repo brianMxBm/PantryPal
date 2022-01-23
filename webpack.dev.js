63e3abd54dde8f39155b267d018b5acd67af94b7
MxBM <officialsmp305@gmail.com>
2022-01-23 03:15:00 -0800

    FIX: Use Eval-Source-Map For Webpack Dev.

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,5 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "development",
-    // bootstrap-select fucks up the bundle when eval-source-map is used.
-    devtool: "source-map",
+    devtool: "eval-source-map",
 });
